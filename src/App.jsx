6b8bafb11a4ba654fd3e16b5051de7209b72e3f7
KatarinaMantay <katarinamantay@gmail.com>
2024-11-19 13:07:53 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.module.css'
+import styles from "./App.module.css";
+import {Navbar} from "./components/navbar/Navbar.jsx";
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <Navbar />
+        </div>
+    );
+}
+
+export default App;
+
